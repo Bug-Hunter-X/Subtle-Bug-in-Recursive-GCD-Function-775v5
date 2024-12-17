a5212b9bdfcb4adba892ac1a5c0498426a9f1333
BugHunter-X <askzenai@gmail.com>
2024-12-17 15:42:40 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+function foo(a, b) {
+  if (a === b) {
+    return true;
+  } else if (a > b) {
+    return foo(a - b, b);
+  } else {
+    return foo(a, b - a);
+  }
+}
+
+console.log(foo(12, 18)); //Output: true
+console.log(foo(13,18)); //Output: false
